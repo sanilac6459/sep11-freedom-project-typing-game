f64ea1435790bebec10805624bc00c527b6d0530
Sanila Chowdhury <sanilac6459@hstat.org>
2022-02-24 23:09:09 +0000

    update

--- sketch.js
+++ sketch.js
@@ -5,5 +5,5 @@ function setup() {
 // when user clicks submit after entering their name
 document.querySelector("button").addEventListener("click", function(){
   var message = document.getElementById("name").value;
-  document.getElementById("demo").innerHTML = "Hello " + message + ", " + "You sure came to the right place to become the world fastest typer! " + "No pressure though, well not yet...";
+  document.getElementById("demo").innerHTML = "Hello " + message + ", " + "you sure came to the right place to become the world fastest typer! " + "No pressure though, well not yet...";
 });
