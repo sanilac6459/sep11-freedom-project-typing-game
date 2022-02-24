600c80baae03d4387b5372b18c2a04f68810f1f8
Sanila Chowdhury <sanilac6459@hstat.org>
2022-02-24 01:50:16 +0000

    update

--- sketch.js
+++ sketch.js
@@ -4,8 +4,13 @@ function setup() {
 }
 
 document.querySelector("#name").addEventListener("keyup", function(event){
-    var message = document.getElementById("#name").value;
-    message.innerHTML = "Hello" + name.value;
+    // var message = document.getElementById("#name").value;
+    // message.innerHTML = "Hello" + name.value;
+
+    var message = document.getElementById("name").value;
+    // message.innerHTML = "Hello" + message;
+    document.getElementById("demo").innerHTML = "Hello " + message + "! " + "You sure came to the right place to become the world fastest typer";
+
 
 });
 
