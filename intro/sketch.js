a2fff77b66835cb18815195e654e6289efb26bf3
Sanila Chowdhury <sanilac6459@hstat.org>
2022-03-01 00:11:48 +0000

    add starting message

--- intro/sketch.js
+++ intro/sketch.js
@@ -6,4 +6,6 @@ function setup() {
 document.querySelector("button").addEventListener("click", function(){
   var message = document.getElementById("name").value;
   document.getElementById("demo").innerHTML = "Hello " + message + ", " + "you sure came to the right place to become the world fastest typer! " + "No pressure though, well not yet...";
+  
+  document.getElementById("start").innerHTML = "If you're ready, please press your spacebar to start. " + "Good luck!"
 });
