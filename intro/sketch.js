38eba5b91096df0284a1514141040b7fd846eb9f
Rajab Begim <rajabb4685@hstat.org>
2022-03-07 23:25:59 +0000

    adddd

--- intro/sketch.js
+++ intro/sketch.js
@@ -3,7 +3,8 @@ function setup() {
 }
 
 // when user clicks submit after entering their name
-document.querySelector("button").addEventListener("click", function(){
-  var message = document.getElementById("name").value;
-  document.getElementById("demo").innerHTML = "Hello " + message + ", " + "you sure came to the right place to become the world fastest typer! " + "No pressure though, well not yet...";
-});
+// document.querySelector("button").addEventListener("click", function(){
+//   var message = document.getElementById("name").value;
+//   document.getElementById("demo").innerHTML = "Hello " + message + ", " + "you sure came to the right place to become the world fastest typer! " + "No pressure though, well not yet...";
+// });
+
