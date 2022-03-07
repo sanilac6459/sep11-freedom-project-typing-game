2797e09ae61392c43946ab9b73a33013e67d1138
Vicky Chen <vickyc6811@hstat.org>
2022-03-07 14:11:45 +0000

    add keypressed

--- intro/sketch.js
+++ intro/sketch.js
@@ -8,3 +8,9 @@ document.addEventListener("click", function(){
   // document.getElementById("demo").innerHTML = "Hello " + message + ", " + "you sure came to the right place to become the world fastest typer! " + "No pressure though, well not yet...";
   document.getElementById("start").innerHTML = "If you're ready, please press your spacebar to start. " + "Good luck!"
 });
+
+ function keyPressed() {
+          if(keyCode == 32) { // when the user clicks their spacebar
+              location.replace("https://www.deltamath.com/")
+          }
+}
