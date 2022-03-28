1cb113447b5a1617559a0c64dca8ada502fd0804
Rajab Begim <rajabb4685@hstat.org>
2022-03-28 13:08:29 +0000

    update

--- intro/sketch.js
+++ intro/sketch.js
@@ -2,9 +2,11 @@ function setup() {
   noCanvas(); // get rids of canva
 }
 
-// when user clicks submit after entering their name
-// document.querySelector("button").addEventListener("click", function(){
-//   var message = document.getElementById("name").value;
-//   document.getElementById("demo").innerHTML = "Hello " + message + ", " + "you sure came to the right place to become the world fastest typer! " + "No pressure though, well not yet...";
-// });
 
+  document.getElementById("start").innerHTML = "Press your spacebar to start";
+
+ function keyPressed() {
+          if(keyCode == 32) { // when the user clicks their spacebar
+              location.replace("https://www.deltamath.com/")
+          }
+}
