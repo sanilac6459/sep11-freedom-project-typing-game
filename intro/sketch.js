60584f31a0e65240d9ef696b35c4ca6ea452e325
Vicky Chen <vickyc6811@hstat.org>
2022-04-14 13:43:23 +0000

    redirect to main

--- intro/sketch.js
+++ intro/sketch.js
@@ -7,6 +7,6 @@ function setup() {
 
  function keyPressed() {
           if(keyCode == 32) { // when the user clicks their spacebar
-              location.replace("https://www.deltamath.com/")
+              location.replace("https://sanilac6459.github.io/sep11-freedom-project-typing-game/main/index.html")
           }
 }
