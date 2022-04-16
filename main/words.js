1d4b651e1c4217292f89e0d13cd89516e64abfb0
Vicky Chen <vickyc6811@hstat.org>
2022-04-16 18:40:14 +0000

    update

---
+++ main/words.js
@@ -0,0 +1,3 @@
+// a string of random words, no punctuations
+
+var wordString = "Bulbasaur Ivysaur Venusaur Charmander Charmeleon Charizard Squirtle Wartortle Blastoise Caterpie Metapod Butterfree Weedle Kakuna Beedrill Pidgey Pidgeotto Pidgeot Rattata" // assign string of words to wordString variable
